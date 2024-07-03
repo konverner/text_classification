aba5942dddaed3145eee83444940bf4438058164
konverner <konst.verner@gmail.com>
2024-07-03 23:26:21 +0200

    change name

--- src/text_classification/api/routes.py
+++ src/text_classification/api/routes.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, HTTPException
 from omegaconf import OmegaConf
 from hydra.utils import instantiate
 
-from text_classification.api.models import PredictionRequest, PredictionResponse
+from text_classification.api.schemas import PredictionRequest, PredictionResponse
 
 # Set up logging
 logger = logging.getLogger(__name__)
@@ -24,21 +24,23 @@ text_classifier = instantiate(config.text_classifier)
 text_preprocessor = instantiate(config.text_preprocessor)
 logger.info("Model loaded successfully.")
 
-@router.post("/predict", response_model=PredictionResponse)
-async def predict(request: PredictionRequest):
+@router.post("/classify", response_model=PredictionResponse)
+async def classify(request: PredictionRequest):
     text = request.text
 
     if not text:
         logger.warning("Received empty text input.")
         raise HTTPException(status_code=400, detail="Text input is empty")
 
-    logger.info(f"Received text for prediction: {text}")
+    logger.info(f"Received text: {text}")
 
     # Preprocess the text
     preprocessed_text = text_preprocessor.preprocess(text)
 
+    logger.info(f"Preprocessed text: {text}")
+
     # Perform prediction
-    result = text_classifier.predict(preprocessed_text)
+    result = text_classifier.predict([preprocessed_text])[0]
     logger.info(f"Prediction result: {result}")
 
     return PredictionResponse(sentiment=result["label"])
